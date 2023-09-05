71c8915702125ff5d9fcae1faff6533ccabe6511
abbasghazal <134511359+abbasghazal@users.noreply.github.com>
2023-09-05 20:20:19 +0300

    Add files via upload

---
+++ shahmMusic/__main__.py
@@ -0,0 +1,60 @@
+
+
+import asyncio
+import importlib
+import sys
+
+from pyrogram import idle
+from pytgcalls.exceptions import NoActiveGroupCall
+
+import config
+from config import BANNED_USERS
+from shahmMusic import LOGGER, app, userbot
+from shahmMusic.core.call import shahm
+from shahmMusic.plugins import ALL_MODULES
+from shahmMusic.utils.database import get_banned_users, get_gbanned
+
+loop = asyncio.get_event_loop()
+
+
+async def init():
+    if (
+        not config.STRING1
+        and not config.STRING2
+        and not config.STRING3
+        and not config.STRING4
+        and not config.STRING5
+    ):
+        LOGGER("shahmMusic").error("Add Pyrogram string session and then try...")
+    try:
+        users = await get_gbanned()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+        users = await get_banned_users()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+    except:
+        pass
+    await app.start()
+    for all_module in ALL_MODULES:
+        importlib.import_module("shahmMusic.plugins" + all_module)
+    LOGGER("shahmMusic.plugins").info("Necessary Modules Imported Successfully.")
+    await userbot.start()
+    await shahm.start()
+    try:
+        await shahm.stream_call("https://telegra.ph/file/b60b80ccb06f7a48f68b5.mp4")
+    except NoActiveGroupCall:
+        LOGGER("shahmMusic").error(
+            "[ERROR] - \n\nTurn on group voice chat and don't put it off otherwise I'll stop working thanks."
+        )
+        sys.exit()
+    except:
+        pass
+    await shahm.decorators()
+    LOGGER("shahmMusic").info("Music Bot Started Successfully")
+    await idle()
+
+
+if __name__ == "__main__":
+    loop.run_until_complete(init())
+    LOGGER("shahmMusic").info("Stopping Music Bot")
