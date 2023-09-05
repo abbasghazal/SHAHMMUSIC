b0fbb7f4f57e0884251369c870b6a43b139d7fd5
abbasghazal <134511359+abbasghazal@users.noreply.github.com>
2023-09-05 20:23:38 +0300

    Add files via upload

---
+++ shahmMusic/utils/theme.py
@@ -0,0 +1,26 @@
+
+
+import random
+from shahmMusic.utils.database import get_theme
+
+themes = [
+    "alexa1",
+    "alexa2",
+    "alexa3",
+    "alexa4",
+    "alexa5",
+    "alexa6",
+    "alexa7",
+    "alexa8",
+]
+
+
+async def check_theme(chat_id: int):
+    _theme = await get_theme(chat_id, "theme")
+    if not _theme:
+        theme = random.choice(themes)
+    else:
+        theme = _theme["theme"]
+        if theme == "Random":
+            theme = random.choice(themes)
+    return theme
