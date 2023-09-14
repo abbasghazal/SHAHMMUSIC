deed97b6f3ad2cd7e6ad0c731f846fc1653fa53f
abbasghazal <134511359+abbasghazal@users.noreply.github.com>
2023-09-14 08:00:10 +0300

    Add files via upload

---
+++ shahmMusic/plugins/tools/ping.py
@@ -0,0 +1,43 @@
+#
+# Copyright (C) 2021-2022 by shahm_Help@Github, < https://github.com/Jankarikiduniya >.
+# A Powerful Music Bot Property Of Rocks Indian Largest Chatting Group
+
+# Kanged By © @Dr_Asad_Ali
+# Rocks © @Shayri_Music_Lovers
+# Owner Asad Ali
+# Harshit Sharma
+# All rights reserved. © Alisha © shahm © Yukki
+
+
+from datetime import datetime
+
+from pyrogram import filters
+from pyrogram.types import Message
+
+from config import BANNED_USERS, MUSIC_BOT_NAME, PING_IMG_URL
+from strings import get_command
+from shahmMusic import app
+from shahmMusic.core.call import shahm
+from shahmMusic.utils import bot_sys_stats
+from shahmMusic.utils.decorators.language import language
+
+### Commands
+PING_COMMAND = get_command("PING_COMMAND")
+
+
+@app.on_message(
+    filters.command(PING_COMMAND) & filters.group & ~filters.edited & ~BANNED_USERS
+)
+@language
+async def ping_com(client, message: Message, _):
+    response = await message.reply_photo(
+        photo=PING_IMG_URL,
+        caption=_["ping_1"],
+    )
+    start = datetime.now()
+    pytgping = await shahm.ping()
+    UP, CPU, RAM, DISK = await bot_sys_stats()
+    resp = (datetime.now() - start).microseconds / 1000
+    await response.edit_text(
+        _["ping_2"].format(resp, MUSIC_BOT_NAME, UP, RAM, CPU, DISK, pytgping)
+    )
