0a923b9fdd26d3bca9e00c531b4973601c916395
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-09-29 18:19:55 +0300

    Update mute.py

--- shahmMusic/plugins/admins/mute.py
+++ shahmMusic/plugins/admins/mute.py
@@ -15,7 +15,7 @@ from pyrogram.types import Message
 from config import BANNED_USERS
 from strings import get_command
 from shahmMusic import app
-from shahmMusic.core.call import shshm
+from shahmMusic.core.call import shahm
 from shahmMusic.utils.database import is_muted, mute_on
 from shahmMusic.utils.decorators import AdminRightsCheck
 
@@ -33,7 +33,7 @@ async def mute_admin(cli, message: Message, _, chat_id):
     if await is_muted(chat_id):
         return await message.reply_text(_["admin_5"], disable_web_page_preview=True)
     await mute_on(chat_id)
-    await shshm.mute_stream(chat_id)
+    await shahm.mute_stream(chat_id)
     await message.reply_text(
         _["admin_6"].format(message.from_user.mention), disable_web_page_preview=True
     )
